da9b8f803fee5dbc18babbfb352434279e49e1f3
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2024-01-23 20:22:42 +0530

    Updated SearchDetails component

--- Medicare/pages/subPages/SearchDetails.tsx
+++ Medicare/pages/subPages/SearchDetails.tsx
@@ -1,31 +1,30 @@
-import {SafeAreaView, ScrollView, Text, View} from "react-native";
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from "react-native";
 import {GoBackButton} from "../../components/GoBackButton.tsx";
 import {DoctorCard} from "../../components/DoctorCard.tsx";
 import {Space} from "../../components/Space.tsx";
 import {SearchBar} from "../../components/SearchBar.tsx";
+import {useState} from "react";
 
 export function SearchDetails() {
+    const [search, setSearch] = useState('');
+    let handleGoBackEvent = () => {
+
+    }
+    let handleSearchEvent = (event: any) => {
+        setSearch(event);
+    }
+
     return (
-        <SafeAreaView style={{flex: 1, backgroundColor: '#e9f3fd'}}>
-            <GoBackButton event={''}/>
-            <ScrollView style={{flex: 1}} contentContainerStyle={{flexGrow: 1}}>
-                <View style={{
-                    flex: 1,
-                    width: '100%',
-                    alignItems: 'center',
-                    justifyContent:'center',
-                }}>
+        <SafeAreaView style={styles.safeAreaView}>
+            <GoBackButton event={handleGoBackEvent}/>
+            <ScrollView style={styles.scrollView} contentContainerStyle={{flexGrow: 1}}>
+                <View style={styles.mainView}>
                     <Space height={80}/>
 
                     <View style={{width: '90%'}}>
-                        <SearchBar value={''} event={''}/>
+                        <SearchBar value={search} event={handleSearchEvent}/>
                         <Space height={40}/>
-                        <View style={{
-                            width: '100%',
-                            flexDirection: 'column',
-                            gap: 10,
-                            alignItems: 'center',
-                        }}>
+                        <View style={styles.doctorCardView}>
                             <DoctorCard name={'J. P. Isuru Dhananjaya'} category={'Cardiologist'}/>
                             <DoctorCard name={'J. P. Isuru Dhananjaya'} category={'Dermatologist'}/>
                             <DoctorCard name={'J. P. Isuru Dhananjaya'} category={'Endocrinologist'}/>
@@ -45,3 +44,20 @@ export function SearchDetails() {
         </SafeAreaView>
     );
 }
+
+let styles = StyleSheet.create({
+    safeAreaView: {flex: 1, backgroundColor: '#e9f3fd'},
+    scrollView: {flex: 1},
+    mainView: {
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    doctorCardView:{
+        width: '100%',
+        flexDirection: 'column',
+        gap: 10,
+        alignItems: 'center',
+    }
+});
